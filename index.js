af34629c6769e128b526e7f2d1aa90342a0181de
play.js <git@gituser.com>
2020-12-15 16:09:38 -0800

    adding comments

--- index.js
+++ index.js
@@ -47,9 +47,12 @@ function PwGen(options)
   let used = ""
   for (i=0;i<len;i++)
   {
+    // make sure we get at least one character 
+    // from each group of specified strings
     let index = Math.round(Math.random()*(opt.length-1))
     if (opt.length>0 && used.length>=0 && used.length!=opt.length)
     {
+      // find an unused index
       while(used.indexOf(index)>=0)
       {
         index=Math.round(Math.random()*(opt.length-1))
@@ -57,9 +60,12 @@ function PwGen(options)
       }
       //print("test index="+index+" used indexes="+used)
     }
+    // only save the index if it hasnt been used
     if (used.indexOf(index)==-1)
       used+=index
     //print("index="+index+" used indexes="+used)
+    // pull a random character out of the selected group 
+    // and add it to the password
     pw+=opts[index][Math.round(Math.random()*(opts[index].length-1))]
 
     //print(pw)
